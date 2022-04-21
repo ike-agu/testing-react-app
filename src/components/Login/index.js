e23da6b627e323d21facadc4ce6ad2fb04bcd60f
Ikenna Agulobi <ikennaww@yahoo.fr>
2022-04-21 07:47:22 +0100

    installed axios and fetched fake data from jsonplaceholder.typicode.com

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,14 +1,17 @@
+import axios from "axios"
 import {useState} from "react"
 
 const Login = () => {
-  const  [error,setError] = useState(false)
-  const  [password,setPassword] = useState("")
-  const  [username,setUserName] = useState("")
+  const  [error,setError] = useState(false);
+  const  [password,setPassword] = useState("");
+  const  [username,setUserName] = useState("");
+  const  [user,setUser] = useState({});
 
   const handleClick = async(e) => {
     e.preventDefault();
     try {
-
+      const {data} = axios.get('https://jsonplaceholder.typicode.com/users/1')
+      setUser(data)
     } catch (error) {
       setError(true)
     }
@@ -16,6 +19,7 @@ const Login = () => {
 
   return (
     <div className="container">
+      <span className="user">{user.name}</span>
     <form>
       <input type="test" placeholder="username"  value={username} onChange={e => setUserName(e.target.value)}/>
       <input type="password" placeholder="password" value={password} onChange={e => setPassword(e.target.value)}/>
